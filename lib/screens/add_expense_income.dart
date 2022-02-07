f7e6bf869b709b0c9c3c433db40e712b75752e5f
Saurav Kumar <saurav.0kmr@gmail.com>
2022-02-08 01:31:13 +0530

    Created some functionalities of bottom sheet in add_income_expense screen.

--- lib/screens/add_expense_income.dart
+++ lib/screens/add_expense_income.dart
@@ -9,15 +9,14 @@ class AddExpenseIncome extends StatefulWidget{
 
 class _AddExpenseIncomeState extends State<AddExpenseIncome>{
 
-  /*void createBottomSheet(){
-    Scaffold.of(context).showBottomSheet<void>(BuildContext context){
-      return Container(
-        child: Center(
-          //chil
-        )
-      );
-    };
-  }*/
+  void createBottomSheet(){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return CreateBottomSheet();
+        }
+    );
+  }
 
   @override
   Widget build(BuildContext context){
@@ -25,144 +24,157 @@ class _AddExpenseIncomeState extends State<AddExpenseIncome>{
       appBar: AppBar(
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 90,
-            padding: const EdgeInsets.all(20),
-            color: Colors.blue,
-            child: Row(
-              children: [
-                RaisedButton(
-                  onPressed: (){
-                  },
-                  child: const Text("Transportation",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.red,
-                      fontWeight: FontWeight.bold
-                    ),
-            ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                const Flexible(
-                  child: TextField(
-                    textAlign: TextAlign.end,
-                    style: TextStyle(fontSize: 25),
-                    decoration: InputDecoration(
-                      hintText: "0",
-                      border: OutlineInputBorder(),
-                    ),
-                  )
-                ),
-              ],
-            ),
-
-          ),
-          Container(padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                GestureDetector(
-                  onTap: (){
-                    print("tapped");
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    height: 45,
-                    color: Colors.grey,
-                    child: Row(
-                      children: const [
-                        Icon(Icons.calendar_today_outlined,
-                          size: 18,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("Date:",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("22 feb"),
-                        Align(
-                          alignment: AlignmentDirectional.centerEnd,
-                          child: Icon(Icons.navigate_next),
-                        ),
-                        //Icon(Icons.navigate_next,),
-                      ],
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    print("tapped time");
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    height: 45,
-                    color: Colors.grey,
-                    child: Row(
-                      children: const [
-                        Icon(Icons.access_time,
-                          size: 18,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("Time:",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text("8:00"),
-                        Align(
-                          alignment: AlignmentDirectional.centerEnd,
-                          child: Icon(Icons.navigate_next),
-                        ),
-                        //Icon(Icons.navigate_next,),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.grey,
-                  height: 45,
-                  child: Row(
-                    children: const [
-                      Icon(Icons.edit),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("Remark: ",
+      body:Builder(
+        builder: (context) {
+          return Column(
+            children: [
+              Container(
+                height: 90,
+                padding: const EdgeInsets.all(20),
+                color: Colors.blue,
+                child: Row(
+                  children: [
+                    RaisedButton(
+                      onPressed: createBottomSheet,
+                      /*onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (context){
+                              return CreateBottomSheet();
+                            }
+                        );
+                      },*/
+                      child: const Text("Transportation",
                         style: TextStyle(
-                          fontWeight: FontWeight.bold
+                            fontSize: 20,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold
                         ),
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Flexible(
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Flexible(
                         child: TextField(
+                          textAlign: TextAlign.end,
+                          style: TextStyle(fontSize: 25),
                           decoration: InputDecoration(
-                            hintText: "Write a note",
+                            hintText: "0",
+                            border: OutlineInputBorder(),
                           ),
+                        )
+                    ),
+                  ],
+                ),
+
+              ),
+              Container(padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        print("tapped");
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        height: 45,
+                        color: Colors.grey,
+                        child: Row(
+                          children: const [
+                            Icon(Icons.calendar_today_outlined,
+                              size: 18,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Date:",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("22 feb"),
+                            Align(
+                              alignment: AlignmentDirectional.centerEnd,
+                              child: Icon(Icons.navigate_next),
+                            ),
+                            //Icon(Icons.navigate_next,),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("tapped time");
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        height: 45,
+                        color: Colors.grey,
+                        child: Row(
+                          children: const [
+                            Icon(Icons.access_time,
+                              size: 18,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Time:",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("8:00"),
+                            Align(
+                              alignment: AlignmentDirectional.centerEnd,
+                              child: Icon(Icons.navigate_next),
+                            ),
+                            //Icon(Icons.navigate_next,),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey,
+                      height: 45,
+                      child: Row(
+                        children: const [
+                          Icon(Icons.edit),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text("Remark: ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Flexible(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Write a note",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          Flexible(
-            child: CreateBottomSheet()
-          )
-        ],
-      ),
+              ),
+              /*Flexible(
+              child: CreateBottomSheet()
+            )*/
+            ],
+          );
+        }
+      )
+
+
     );
   }
 
@@ -178,6 +190,83 @@ class CreateBottomSheet extends StatefulWidget{
 class _CreateBottomSheetState extends State<CreateBottomSheet>{
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return ListView(
+      children: [
+        GestureDetector(
+          child: CategoryCard(category: "Food"),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        GestureDetector(
+          child: CategoryCard(category: "Fruits"),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        GestureDetector(
+          child: CategoryCard(category: "Transportation"),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        GestureDetector(
+          child: CategoryCard(category: "Gym"),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        GestureDetector(
+          child: CategoryCard(category: "Gifts"),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        GestureDetector(
+          child: CategoryCard(category: "Travel"),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget{
+  const CategoryCard({
+    required this.category,
+    Key? key,
+  }): super(key: key);
+  final String category;
+
+  @override
+  Widget build(BuildContext context){
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Container(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        height: 70,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Text(category,
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            /*const Expanded(
+              flex: 1,
+              child: Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Icon(Icons.edit),
+              ),
+            )*/
+          ],
+        ),
+      ),
+    );
   }
 }
